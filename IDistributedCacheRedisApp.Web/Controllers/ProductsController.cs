c7839bd972030f3cb1a867df139e8c698435426c
BatuhanBedir <114083704+BatuhanBedir@users.noreply.github.com>
2023-08-18 16:17:46 +0300

    distributedCache-get/getstring-set/setstring-remove-filecach

--- IDistributedCacheRedisApp.Web/Controllers/ProductsController.cs
+++ IDistributedCacheRedisApp.Web/Controllers/ProductsController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using IDistributedCacheRedisApp.Web.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace IDistributedCacheRedisApp.Web.Controllers;
 
@@ -14,19 +17,35 @@ public class ProductsController : Controller
 
     public async Task<IActionResult> CacheSetString()
     {
-        await _distributedCache.SetStringAsync("name", "Batuhan", new DistributedCacheEntryOptions
-        {
-            AbsoluteExpiration = DateTime.Now.AddMinutes(1)
-        }) ;
+        Product product = new Product { Id = 1, Name = "Kalem1", Price = 100 };
+
+        string jsonProduct =JsonConvert.SerializeObject(product);
+
+        //json
+        //await _distributedCache.SetStringAsync("product:1", jsonProduct, new DistributedCacheEntryOptions
+        //{
+        //    AbsoluteExpiration = DateTime.Now.AddMinutes(30)
+        //});
+
+        //byte
+        Byte[] byteProduct = Encoding.UTF8.GetBytes(jsonProduct);
+        await _distributedCache.SetAsync("product:1", byteProduct);
 
         return View();
     }
 
     public async Task<IActionResult> CacheGetString()
     {
-        var name =await _distributedCache.GetStringAsync("name");
+        //byte
+        Byte[] byteProduct =await _distributedCache.GetAsync("product:1");
+        string jsonProduct = Encoding.UTF8.GetString(byteProduct);
 
-        ViewBag.name = name;
+        //json
+        //var jsonProduct = await _distributedCache.GetStringAsync("product:1");
+
+        Product product = JsonConvert.DeserializeObject<Product>(jsonProduct);
+
+        ViewBag.product = product;
 
         return View();
     }
@@ -36,5 +55,25 @@ public class ProductsController : Controller
         await _distributedCache.RemoveAsync("name");
 
         return View();
+    }
+
+    public IActionResult CacheFile()
+    {
+        string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images/Pikachu.jpg");
+
+        byte[] imageByte = System.IO.File.ReadAllBytes(path);
+
+        _distributedCache.Set("image", imageByte, new DistributedCacheEntryOptions
+        {
+            AbsoluteExpiration = DateTime.Now.AddMinutes(30)
+        });
+
+        return View();
+    }
+    public IActionResult CacheFileRead()
+    {
+        byte[] imageByte = _distributedCache.Get("image");
+
+        return File(imageByte, "image/jpg");
     }
 }
