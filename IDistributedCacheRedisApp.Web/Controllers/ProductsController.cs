60bd1056e97969effdfb6018a98a71e67138c6e7
BatuhanBedir <114083704+BatuhanBedir@users.noreply.github.com>
2023-08-18 15:33:35 +0300

    Add project files.

---
+++ IDistributedCacheRedisApp.Web/Controllers/ProductsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace IDistributedCacheRedisApp.Web.Controllers;
+
+public class ProductsController : Controller
+{
+    private IDistributedCache _distributedCache;
+
+    public ProductsController(IDistributedCache distributedCache)
+    {
+        _distributedCache = distributedCache;
+    }
+
+    public async Task<IActionResult> CacheSetString()
+    {
+        await _distributedCache.SetStringAsync("name", "Batuhan", new DistributedCacheEntryOptions
+        {
+            AbsoluteExpiration = DateTime.Now.AddMinutes(1)
+        }) ;
+
+        return View();
+    }
+
+    public async Task<IActionResult> CacheGetString()
+    {
+        var name =await _distributedCache.GetStringAsync("name");
+
+        ViewBag.name = name;
+
+        return View();
+    }
+
+    public async Task<IActionResult> CacheRemove()
+    {
+        await _distributedCache.RemoveAsync("name");
+
+        return View();
+    }
+}
